d79645f79a1c0fa0ef16b5a521404e531b373a2b
Julius <juliusblusevicius20@gmail.com>
2019-07-08 11:54:51 +0300

    External data and  $http

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+myApp.controller('mainController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
 
-    $scope.alertClick = function() {
-        alert("Clicked!");
+    $scope.handle = '';
+
+    $scope.lowercasehandle = function () {
+        return $filter('lowercase')($scope.handle);
     };
 
-    $scope.name = 'John Doe';
+    $scope.characters = 5;
+
+    $http.get('/api')
+        .success(function(result){
+             $scope.rules = result;
+         })
+        .error(function (data, status){
+
+            console.log(data)
+        });
+
+        $scope.newRule = '';
+        $scope.addRule = function() {
+
+            $http.post('/api', {newRule : $scope.newRule })
+                .success(function (result){
+                    $scope.rules = result;
+                    $scope.newRule = '';
+            })
+                .error(function (data, status){
+
+                    console.log(data);
+            });
+        }
 
 }]);
